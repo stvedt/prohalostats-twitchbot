27b47daec741a21bcfe2fcf435ae7b273510533f
Stephen Tvedt <stvedt@kbsp.com>
2015-12-07 18:04:14 -0500

    update helpers to map teams

--- helpers.js
+++ helpers.js
@@ -5,11 +5,8 @@ var scrimsData = require('./data/scrims');
 
 module.exports = {
     getTeams: function(scrimID) {
-        var scrim = scrimsData[scrimID];
-        var teamNames = [];
-        for ( property in scrim) {
-            teamNames.push(property);
-        }
+        var scrim = scrimsData.find(function (res) { return res.id === scrimID; });
+        var teamNames = [scrim.team1.name, scrim.team2.name]
         return teamNames;
     }
 };
